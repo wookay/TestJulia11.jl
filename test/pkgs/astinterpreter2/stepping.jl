7f33dd534f9eb18ed6b1d46763d5b905e37a0165
WooKyoung Noh <wookay.noh@gmail.com>
2019-02-01 01:01:10 +0900

    astinterpreter2

--- test/pkgs/astinterpreter2/stepping.jl
+++ test/pkgs/astinterpreter2/stepping.jl
@@ -2,23 +2,24 @@ module test_pkgs_astinterpreter2
 
 using Test
 using ASTInterpreter2
-using ASTInterpreter2: enter_call_expr
+
+# https://github.com/JuliaDebug/ASTInterpreter2.jl/blob/master/test/utils.jl#L6
+using Base.Meta: isexpr
+using ASTInterpreter2: JuliaStackFrame
+using ASTInterpreter2: pc_expr, plain, evaluate_call!, finish_and_return!, @eval_rhs
 using DebuggerFramework
 using DebuggerFramework: execute_command
-using Base.Meta # isexpr
 
-function CallTest()
-    UnitRange{Int64}(2,2)
-end
-
+# Steps through the whole expression using `s`
 function step_through(frame)
     state = DebuggerFramework.dummy_state([frame])
-    while !isexpr(ASTInterpreter2.pc_expr(state.stack[end]), :return)
+    while !isexpr(plain(pc_expr(state.stack[end])), :return)
         execute_command(state, state.stack[1], Val{:s}(), "s")
     end
-    return ASTInterpreter2.lookup_var_if_var(state.stack[end], ASTInterpreter2.pc_expr(state.stack[end]).args[1])
+    lastframe = state.stack[end]
+    return @eval_rhs(true, lastframe, plain(pc_expr(lastframe)).args[1], lastframe.pc[])
 end
 
-@test step_through(ASTInterpreter2.enter_call_expr(:($(+)(1,2)))) == 3
+@test step_through(ASTInterpreter2.enter_call_expr(:($(+)(1,2.5)))) == 3.5
 
 end # module test_pkgs_astinterpreter2
