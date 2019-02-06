4c9175236aa9d5f9c6a460881bf3924ad1289690
WooKyoung Noh <wookay.noh@gmail.com>
2019-02-07 00:06:08 +0900

    (f ∘ g)'(x)

--- test/pkgs/zygote/test.jl
+++ test/pkgs/zygote/test.jl
@@ -7,4 +7,9 @@ f(x) = sin(x) + 1
 @test f' isa Function
 @test f'(1) == 0.5403023058681398
 
+g(x) = cos(x) + 1
+@test g'(1) == -0.8414709848078965
+@test (f ∘ g)'(1) == -0.02565585722112011
+@test f'(g(1)) * g'(1) == (f ∘ g)'(1)
+
 end # module test_pkgs_zygote
