36aa46c3ef4e21a3d9e17027fff8c5a5033a3ee1
WooKyoung Noh <wookay.noh@gmail.com>
2019-03-09 16:05:06 +0900

    zygote

--- test/pkgs/zygote/test.jl
+++ test/pkgs/zygote/test.jl
@@ -1,5 +1,4 @@
-using Jive
-@If VERSION < v"1.2.0-DEV" module test_pkgs_zygote
+module test_pkgs_zygote_adjoint # '
 
 using Test
 using Zygote
@@ -13,4 +12,34 @@ g(x) = cos(x) + 1
 @test (f ∘ g)'(1) == -0.02565585722112011
 @test f'(g(1)) * g'(1) == (f ∘ g)'(1)
 
-end # module test_pkgs_zygote
+end # module test_pkgs_zygote_adjoint
+
+
+module test_pkgs_zygote_gradient
+
+using Test
+using Zygote
+
+# http://fluxml.ai/Zygote.jl/dev/#Taking-Gradients-1
+
+f(x) = 3x^2 + 2x + 1
+@test gradient(f, 5) == (f'(5),)
+
+y, back = Zygote.forward(f, 5)
+@test y == 86
+@test back(1) == (32,)
+
+end # module test_pkgs_zygote_gradient
+
+
+module test_pkgs_zygote_forwarddiff
+
+using Test
+using Zygote: gradient, forwarddiff
+
+f(x) = 3x^2 + 2x + 1
+@test gradient(5) do x
+    forwarddiff(f, x)
+end == (32,)
+
+end # test_pkgs_zygote_forwarddiff
