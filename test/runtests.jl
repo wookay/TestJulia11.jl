28a5fee4dbf18c9e8cabf55da9a52d9e3aeeae82
WooKyoung Noh <wookay.noh@gmail.com>
2019-04-12 20:29:13 +0900

    Julia 1.3-DEV

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Jive
-julia12skip = []
-runtests(@__DIR__, skip=[julia12skip..., "revise.jl"])
-!isempty(julia12skip) && printstyled("skipped julia 1.2: ", join(julia12skip, ", "), "\n", color=:yellow)
+julia13skip = ["pkgs/irtools", "pkgs/zygote"]
+runtests(@__DIR__, skip=[julia13skip..., "revise.jl"])
+!isempty(julia13skip) && printstyled("skipped julia 1.3: ", join(julia13skip, ", "), "\n", color=:yellow)
 !isempty(Jive.Skipped.expressions) && printstyled("skipped tests: ", join(Jive.Skipped.expressions, ", "), "\n", color=:yellow)
