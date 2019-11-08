dfe976fe8d4d6d6b5c2c629604cbba244a63970c
WooKyoung Noh <wookay.noh@gmail.com>
2019-11-08 23:17:04 +0900

    skip zygote

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Jive
 julia13skip = ["pkgs/irtools", "pkgs/diffeqflux"]
 VERSION <= v"1.2" && push!(julia13skip, "julia/unary")
+VERSION >= v"1.4-DEV" && push!(julia13skip, "pkgs/zygote")
 runtests(@__DIR__, skip=[julia13skip..., "revise.jl"])
 !isempty(julia13skip) && printstyled("skipped julia 1.3: ", join(julia13skip, ", "), "\n", color=:yellow)
 !isempty(Jive.Skipped.expressions) && printstyled("skipped tests: ", join(Jive.Skipped.expressions, ", "), "\n", color=:yellow)
