9d8d327be6fe793b003964b8a4da0bc408e81795
WooKyoung Noh <wookay.noh@gmail.com>
2020-02-25 02:15:29 +0900

    ⨟

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ julia13skip = ["pkgs/irtools", "pkgs/diffeqflux"]
 VERSION <= v"1.2" && push!(julia13skip, "julia/unary")
 VERSION >= v"1.4-DEV" && push!(julia13skip, "pkgs/zygote")
 VERSION < v"1.4.0-DEV.599" && push!(julia13skip, "julia/abstractarray.jl")
+VERSION < v"1.5.0-DEV.302" && push!(julia13skip, "julia/operators_bbsemi.jl")
 haskey(ENV, "TRAVIS") && push!(julia13skip, "pkgs/poirot")
 runtests(@__DIR__, skip=[julia13skip..., "revise.jl"])
 !isempty(julia13skip) && printstyled("skipped julia 1.3: ", join(julia13skip, ", "), "\n", color=:yellow)
