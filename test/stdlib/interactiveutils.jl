73760b83fae6b587281d433a167f92427672612f
WooKyoung Noh <wookay.noh@gmail.com>
2020-03-11 20:09:52 +0900

    supertypes

--- test/stdlib/interactiveutils.jl
+++ test/stdlib/interactiveutils.jl
@@ -5,5 +5,6 @@ using Test
 using InteractiveUtils
 
 @test supertypes(typeof(+)) == (typeof(+), Function, Any)
+@test supertypes(Cfloat) == (Float32, AbstractFloat, Real, Number, Any)
 
 end # module test_stdlib_interactiveutils
