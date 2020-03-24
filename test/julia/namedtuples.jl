f709a49aeef0ad1c89ef06f0cf34a86d6c9ddd5d
WooKyoung Noh <wookay.noh@gmail.com>
2020-03-25 04:07:52 +0900

    implicit named tuple

--- test/julia/namedtuples.jl
+++ test/julia/namedtuples.jl
@@ -1,8 +1,18 @@
 using Jive
-@If VERSION >= v"1.5.0-DEV.215" module test_julia_namedtuple
+@If VERSION >= v"1.5.0-DEV.215" module test_julia_namedtuple_215
 
 using Test
 
 @test (;) == NamedTuple()
 
-end # module test_julia_namedtuple
+end # module test_julia_namedtuple_215
+
+
+@If VERSION >= v"1.5.0-DEV.499" module test_julia_namedtuple_499
+
+using Test
+
+x = 0
+@test (; x) == (; x=0)
+
+end # module test_julia_namedtuple_499
