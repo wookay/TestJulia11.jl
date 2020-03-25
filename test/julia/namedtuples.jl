5d1e67c6867f24064805336116937f9d702945fd
WooKyoung Noh <wookay.noh@gmail.com>
2020-03-25 15:58:02 +0900

    implicit names

--- test/julia/namedtuples.jl
+++ test/julia/namedtuples.jl
@@ -15,4 +15,13 @@ using Test
 x = 0
 @test (; x) == (; x=0)
 
+struct S
+    val
+end
+
+@test (; S(1).val) == (; val=1) == (val=1,)
+
+f(; val) = val + 2
+@test f(; S(1).val) == 3
+
 end # module test_julia_namedtuple_499
