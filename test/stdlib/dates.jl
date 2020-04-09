b3bb97f82cb018d088d7c7709ecd10dbe3e3a60b
WooKyoung Noh <wookay.noh@gmail.com>
2020-04-09 16:49:38 +0900

    dates

--- test/stdlib/dates.jl
+++ test/stdlib/dates.jl
@@ -26,7 +26,7 @@ using Test
 using Dates
 
 @test eps(Date) == Day(1)
-@test eps(Time) == Nanosecond(1)
 @test eps(DateTime) == Millisecond(1)
+@test eps(Time) == Nanosecond(1)
 
 end # module test_stdlib_dates_eps
