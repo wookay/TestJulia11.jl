341040ca380b98fa0785d1db2971089d003d0c85
WooKyoung Noh <wookay.noh@gmail.com>
2020-10-08 01:23:44 +0900

    Base.BroadcastFunction

---
+++ test/julia/broadcast.jl
@@ -0,0 +1,11 @@
+using Jive
+@If VERSION >= v"1.6.0-DEV.1144" module test_julia_broadcast
+
+using Test
+
+@test (.+) isa Base.BroadcastFunction
+
+f(x) = 3x
+@test f.([1, 2, 3]) == Base.BroadcastFunction(f)([1, 2, 3]) == [3, 6, 9]
+
+end # module test_julia_broadcast
