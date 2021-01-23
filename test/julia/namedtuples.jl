32b15306108ef540a94500bd59dddd531308504d
WooKyoung Noh <wookay.noh@gmail.com>
2021-01-23 23:58:40 +0900

    property destructuring

--- test/julia/namedtuples.jl
+++ test/julia/namedtuples.jl
@@ -16,3 +16,20 @@ f(; val) = val + 2
 @test f(; S(1).val) == 3
 
 end # module test_julia_namedtuple_499
+
+
+
+@If VERSION >= v"1.7.0-DEV.364" module test_julia_property_destructuring
+
+using Test
+
+struct X
+    a
+    b
+end
+
+(; a, b) = X(1, 2)
+@test a == 1
+@info b == 2
+
+end # module test_julia_property_destructuring
