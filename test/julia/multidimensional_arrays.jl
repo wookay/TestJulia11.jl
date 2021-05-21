cca3f63ee5a213ee417b29013034837f3e1164da
WooKyoung Noh <wookay.noh@gmail.com>
2021-05-22 01:43:42 +0900

    ndims

--- test/julia/multidimensional_arrays.jl
+++ test/julia/multidimensional_arrays.jl
@@ -7,6 +7,11 @@ using Test
 if VERSION >= v"1.7.0-DEV.1129"
 @test [1;; 2;; 3;; 4] isa Array{Int, 2}
 @test [1;;; 2;;; 3;;; 4] isa Array{Int, 3}
+@test ndims(zeros()) == ndims(ones()) == 0
+@test ndims([0 ;]) == 1
+@test ndims([0 ;;]) == 2
+@test ndims([0 ;;;]) == 3
+@test ndims([0 ;;;;;;;;;;]) == 10
 end # if VERSION >= v"1.7.0-DEV.1129"
 
 end # module test_julia_multidimensional_arrays
