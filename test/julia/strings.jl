0a1e9264c8d381ff5af46852c50eee5bf1bf07b5
WooKyoung Noh <wookay.noh@gmail.com>
2021-06-08 14:23:20 +0900

    replace

--- test/julia/strings.jl
+++ test/julia/strings.jl
@@ -1,15 +1,19 @@
 module test_julia_strings
 
 using Test
-using Jive
 
-@If VERSION >= v"1.5.0-DEV.639" begin
+if VERSION >= v"1.5.0-DEV.639"
 @test contains("ppl").(["apple", "banana"]) == [true, false]
 end
 
-@If VERSION >= v"1.6.0-DEV.63" begin
+if VERSION >= v"1.6.0-DEV.63"
 x = "abc"
+@test view(x, 1:2) == "ab"
 @test view(x, 1:2) isa SubString
+end
+
+if VERSION >= v"1.7.0-DEV.1286"
+@test replace("abc", "a" => "b", "b" => "a") == "bac"
 end
 
 end # module test_julia_strings
