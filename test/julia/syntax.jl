ec56775ddf7fb9a27a776121f5cfcfe3414ba395
WooKyoung Noh <wookay.noh@gmail.com>
2021-07-14 14:40:57 +0900

    test nargs

--- test/julia/syntax.jl
+++ test/julia/syntax.jl
@@ -12,4 +12,21 @@ using Test
 (a, b, c, f()...) = (1, 2, 3)
 @test f() == ()
 
-end # module test_julia_syntax
+end # @If VERSION >= v"1.8.0-DEV.118" module test_julia_syntax
+
+
+
+@If VERSION >= v"1.8.0-DEV.178" module test_julia_syntax_nargs
+
+using Test
+
+const nargs_max = 520000
+ex = Expr(:tuple)
+ex.args = fill!(Vector{Any}(undef, nargs_max), 1)
+tup = eval(ex)
+@test length(tup) == nargs_max
+
+ex.args = fill!(Vector{Any}(undef, nargs_max+1), 1)
+@test_throws ErrorException("syntax: expression too large") eval(ex)
+
+end # @If VERSION >= v"1.8.0-DEV.178" module test_julia_syntax_nargs
