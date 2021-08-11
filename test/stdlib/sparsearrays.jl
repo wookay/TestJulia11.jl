d6cbe958152c82e6a344d23c98236ebade14018d
WooKyoung Noh <wookay.noh@gmail.com>
2021-08-11 14:42:51 +0900

    show sparse

--- test/stdlib/sparsearrays.jl
+++ test/stdlib/sparsearrays.jl
@@ -8,7 +8,9 @@ using SparseArrays
 a = sparse([100, 100, 200, 300])
 @test indexin(a, [100]) == [1, 1, nothing, nothing]
 
-if VERSION >= v"1.6.0-DEV.78"
+if VERSION >= v"1.7.0-DEV.1299"
+@test sprint(show, sparse(reshape(1:9,3,:))) == "sparse([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 1, 1, 2, 2, 2, 3, 3, 3], [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3)"
+elseif VERSION >= v"1.6.0-DEV.78"
 @test sprint(show, sparse(reshape(1:9,3,:))) == "\n 1  4  7\n 2  5  8\n 3  6  9"
 end
 
