7d3d76af570d92d610a7ffdc854ebbb48602e909
WooKyoung Noh <wookay.noh@gmail.com>
2022-03-15 10:53:28 +0900

    setproperty! for modules

--- test/julia/modules.jl
+++ test/julia/modules.jl
@@ -8,3 +8,24 @@ if VERSION >= v"1.8.0-DEV.8"
 end
 
 end # module test_julia_modules
+
+
+module test_julia_modules_property
+
+using Test
+
+module M
+v = 1
+end # module M
+
+@test getproperty(M, :v) == 1
+
+if VERSION >= v"1.9.0-DEV.187"
+    setproperty!(M, :v, 2)
+    @test getproperty(M, :v) == 2
+
+    setglobal!(M, :v, 3)
+    getglobal(M, :v) == 3
+end
+
+end # module test_julia_modules_property
