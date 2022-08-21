7fd2b2d36bf9d131d3701d0f4243fddd7641f918
WooKyoung Noh <wookay.noh@gmail.com>
2022-08-21 17:25:35 +0900

    stack

--- test/julia/abstractarray.jl
+++ test/julia/abstractarray.jl
@@ -12,3 +12,13 @@ A = [5,6,7]
 @test lastindex(A) == 3
 
 end # module test_julia_abstractarray_begin_end
+
+
+@If VERSION >= v"1.9.0-DEV.1163" module test_julia_abstractarray_stack
+
+using Test
+
+@test stack((1:3, 5:7)) == [1  5; 2  6; 3  7]
+@test stack((1:3, 5:7), dims=1) == [1  2  3; 5  6  7]
+
+end # test_julia_abstractarray_stack
