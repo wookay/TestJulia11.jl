b39e43856f95c900c3c9d2f9b76f25571763ba00
WooKyoung Noh <wookay.noh@gmail.com>
2023-10-28 18:04:50 +0900

    fix Core.Compiler.verify_ir

--- test/julia/compiler.jl
+++ test/julia/compiler.jl
@@ -7,9 +7,11 @@ ir = Core.Compiler.inflate_ir(ci)
 @test Core.Compiler.verify_ir(ir) === nothing
 
 if VERSION >= v"1.9.0-DEV.1611"
-using Core.Compiler: IRCode
-ir = IRCode()
-@test Core.Compiler.verify_ir(ir) === nothing
+    using Core.Compiler: IRCode
+    ir = IRCode()
+    if VERSION >= v"1.11"
+        @test_throws ErrorException Core.Compiler.verify_ir(ir)
+    end
 end
 
 end # module test_julia_compiler
