0910d6a5ceb41fd5725b54f578e5902265537ee3
WooKyoung Noh <wookay.noh@gmail.com>
2025-02-19 13:54:02 +0900

    test anonymous module changes in v"1.13"

--- test/julia/modules.jl
+++ test/julia/modules.jl
@@ -3,8 +3,10 @@ module test_julia_modules
 using Test
 
 if VERSION >= v"1.8.0-DEV.8"
-@test !isdefined(Module(:anonymous, false, true), :Core)
-@test !isdefined(Module(:anonymous, false, false), :Core)
+    if VERSION >= v"1.13"
+        @test !isdefined(Module(:anonymous, false, true), :Core)
+    end
+    @test !isdefined(Module(:anonymous, false, false), :Core)
 end
 
 end # module test_julia_modules
