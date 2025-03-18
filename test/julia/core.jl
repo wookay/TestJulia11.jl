507b35d5506351ff15b540c2d1c507b7a066a949
WooKyoung Noh <wookay.noh@gmail.com>
2025-03-18 17:29:14 +0900

    test public, timing fields

--- test/julia/core.jl
+++ test/julia/core.jl
@@ -14,3 +14,58 @@ setglobal!(md, :n, 2)
 end
 
 end # module test_julia_core_setglobal
+
+
+# doc/src/devdocs/ast.md
+"""
+  * Timing fields
+
+    - `time_infer_total`: Total cost of computing `inferred` originally as wall-time from start to finish.
+
+    - `time_infer_cache_saved`: The cost saved from `time_infer_total` by having caching.
+      Adding this to `time_infer_total` should give a stable estimate for comparing the cost
+      of two implementations or one implementation over time. This is generally an
+      over-estimate of the time to infer something, since the cache is frequently effective
+      at handling repeated work.
+
+    - `time_infer_self`: Self cost of julia inference for `inferred` (a portion of
+      `time_infer_total`). This is simply the incremental cost of compiling this one method,
+      if given a fully populated cache of all call targets, even including constant
+      inference results and LimitedAccuracy results, which generally are not in a cache.
+
+    - `time_compile`: Self cost of llvm JIT compilation (e.g. of computing `invoke` from
+      `inferred`). A total cost estimate can be computed by walking all of the `edges`
+      contents and summing those, while accounting for cycles and duplicates. (This field
+      currently does not include any measured AOT compile times.)
+"""
+module test_julia_core_timing_fields
+
+using InteractiveUtils
+using Test
+
+if VERSION >= v"1.13.0-DEV.242"
+
+w::Core.Method = which(÷, (Int32, UInt16))
+mi::Core.MethodInstance = w.specializations[6]
+@test !isdefined(mi, :cache)
+
+code_native(devnull, ÷, dump_module=false, (Int32, UInt16))
+b::Core.CodeInstance = mi.cache
+
+# these time fields have units of seconds
+# (60 ns minimum resolution and 18 hour maximum saturates to Infinity)
+# and are stored in Float16 format
+#    uint16_t time_infer_total;       // total cost of computing `inferred` originally
+#    uint16_t time_infer_cache_saved; // adjustment to total cost,
+#                                        reflecting how much time was saved by having caches,
+#                                        to give a stable real cost without caches for comparisons
+#    uint16_t time_infer_self;        // self cost of julia inference for `inferred` (included in time_infer_total)
+#    _Atomic(uint16_t) time_compile;  // self cost of llvm compilation (e.g. of computing `invoke`)
+time_infer_total       = reinterpret(Float16, b.time_infer_total)
+time_infer_cache_saved = reinterpret(Float16, b.time_infer_cache_saved)
+time_infer_self        = reinterpret(Float16, b.time_infer_self)
+time_compile           = reinterpret(Float16, b.time_compile)
+
+end
+
+end # module test_julia_core_timing_fields
--- test/julia/syntax_public.jl
+++ test/julia/syntax_public.jl
@@ -1,5 +1,13 @@
 using Jive
-@If VERSION >= v"1.12.0-DEV.163" module test_julia_syntax_public
+@If VERSION >= v"1.11" module test_julia_syntax_public
+
+#=
+Julia v1.11
+`public` is a new keyword. Symbols marked with `public` are considered public
+API. Symbols marked with `export` are now also treated as public API. The
+difference between `public` and `export` is that `public` names do not become
+available when `using` a package/module.
+=#
 
 module Mod
     export foo
@@ -7,7 +15,7 @@ module Mod
 
 const foo = 1
 const bar = 2
-end
+end # module Mod
 
 using Test
 
@@ -23,15 +31,28 @@ using Test
 
 module M2
 using ..Mod: foo, bar
-end
-
-@test names(M2; usings=false) == [:M2]
-m2_names_usings_true = names(M2; usings=true)
-@test :foo in m2_names_usings_true
-@test :bar in m2_names_usings_true
-@test :+   in m2_names_usings_true
+end # module M2
 
 @test names(M2; imported=false) == [:M2]
 @test names(M2; imported=true) == [:M2]
 
-end # @If VERSION >= v"1.12.0-DEV.163" module test_julia_syntax_public
+if VERSION >= v"1.12.0-DEV.163"
+    @test names(M2; usings=false) == [:M2]
+    m2_names_usings_true = names(M2; usings=true)
+    @test :foo in m2_names_usings_true
+    @test :bar in m2_names_usings_true
+    @test :+   in m2_names_usings_true
+end
+
+
+module M3
+
+using Test
+using ..Mod
+
+@test foo == 1                   # export foo
+@test_throws UndefVarError bar   # public bar
+
+end # module M3
+
+end # @If VERSION >= v"1.11" module test_julia_syntax_public
